a52744c6f1a782955faaf1d67fa7def8dbc7dd00
unknown <568385476@qq.com>
2023-03-21 12:09:40 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+// use lazy_static::lazy_static;
+
+// use regex::Regex;
+
+fn main() {
+  println!("Hello, world!");
+
+  // lazy_static! {
+  //   static ref RE: Regex = Regex::new(
+  //   r"(?xs)
+  //     (?P<login>login) |
+  //     (?P<register>register) |
+  //     (?P<fuck>fuck)
+  //     (?<comment>/* .*? */)
+  //   "
+  //   )
+  //   .unwrap();
+  // }
+
+  // RE.campture
+
+
+}
