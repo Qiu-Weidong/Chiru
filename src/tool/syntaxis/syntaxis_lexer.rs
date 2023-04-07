99bc0efbfe4227d07e4033a913606f412ae7ca49
unknown <568385476@qq.com>
2023-04-07 11:09:07 +0800

    .

--- src/tool/syntaxis/syntaxis_lexer.rs
+++ src/tool/syntaxis/syntaxis_lexer.rs
@@ -16,64 +16,56 @@ pub struct SyntaxisLexer {
   pub position: Position,
 }
 
+#[allow(dead_code)]
 impl SyntaxisLexer {
   // 前两个是开始符号和结束符号
   pub const _START: usize = 0;
   pub const _STOP: usize = 1;
 
-  pub const LINE_COMMENT: usize=2;
-  pub const BLOCK_COMMENT: usize=3;
-  pub const WS: usize=4;
-  pub const FRAGMENT: usize=5;
-  pub const TOKEN_REF: usize=6;
-  pub const RULE_REF: usize=7;
-  pub const COLON: usize=8;
-  pub const OR: usize=9;
-  pub const SEMI: usize=10;
-  pub const STAR: usize=11;
-  pub const PLUS: usize=12;
-  pub const QUESTION: usize=13;
-  pub const DOT: usize=14;
-  pub const NOT: usize=15;
-  pub const LPAREN: usize=16;
-  pub const RPAREN: usize=17;
-  pub const POUND: usize=18;
-  pub const STRING_LITERAL: usize=19;
-  pub const RANGE: usize=20;
-  pub const REGULAR_LITERAL: usize=21;
-  pub const EPSILON: usize=22;
+  // 从这里开始使用模板
+  
+  pub const RULE_REF: usize = 2;
+  pub const TOKEN_REF: usize = 3;
+  pub const COLON: usize = 4;
+  pub const SEMI: usize = 5;
+  pub const OR: usize = 6;
+  pub const EPSILON: usize = 7;
+  pub const STAR: usize = 8;
+  pub const PLUS: usize = 9;
+  pub const QUESTION: usize = 10;
+  pub const LPAREN: usize = 11;
+  pub const RPAREN: usize = 12;
+  pub const STRING_LITERAL: usize = 13;
+  pub const REGULAR_LITERAL: usize = 14;
+  pub const WHITE_SPACE: usize = 15;
 
 
   pub fn new(input: &str) -> Self {
     let regex_list = vec![
 
-      Regex::new(r##"^//.*?\n"##).unwrap(), // LINE_COMMENT
-      Regex::new(r##"^(?s)/\*.*?\*/"##).unwrap(), // BLOCK_COMMENT
-      Regex::new(r##"^[ \t\r\n]+"##).unwrap(), // WS
-      Regex::new(r##"^fragment"##).unwrap(), // fragment
-      Regex::new(r##"^[A-Z][a-zA-Z0-9_]+"##).unwrap(), // TOKEN_REF
-      Regex::new(r##"^[a-z][a-zA-Z0-9_]+"##).unwrap(), // RULE_REF
-      Regex::new(r##"^(::=|:=|->|=>|:|=)"##).unwrap(), // COLON
-      Regex::new(r##"^\|"##).unwrap(), // OR
-      Regex::new(r##"^;"##).unwrap(), // SEMI
-      Regex::new(r##"^\*"##).unwrap(), // STAR
-      Regex::new(r##"^\+"##).unwrap(), // PLUS
-      Regex::new(r##"^\?"##).unwrap(), // QUESTION
-      Regex::new(r##"^\."##).unwrap(), // DOT
-      Regex::new(r##"^\~"##).unwrap(), // NOT
-      Regex::new(r##"^\("##).unwrap(), // LPAREN
-      Regex::new(r##"^\)"##).unwrap(), // RPAREN
-      Regex::new(r##"^#"##).unwrap(), // POUND
-      Regex::new(r##"^'((\\\\|\\'|\\a|\\d|\\n|\\r|\\t|\\f|\\v|\\u\{(0x|0)?[a-f0-9]+\})|\d|[^\a\d\n\r\t\f\v\\'])*'"##).unwrap(), // STRING_LITERAL
-      Regex::new(r##"^\.\."##).unwrap(), // RANGE
-      Regex::new(r##"^/(\\/|[^/])+/"##).unwrap(), // REGULAR_LITERAL
-      Regex::new(r##"^(ε|epsilon)"##).unwrap(), // EPSILON
+      // 这里也需要使用模板
+      
+      Regex::new(r####"^([a-z][a-zA-Z0-9_]+)"####).unwrap(), // RULE_REF
+      Regex::new(r####"^([A-Z][a-zA-Z0-9_]+)"####).unwrap(), // TOKEN_REF
+      Regex::new(r####"^(::=|:=|->|=>|:|=)"####).unwrap(), // COLON
+      Regex::new(r####"^(;)"####).unwrap(), // SEMI
+      Regex::new(r####"^(\|)"####).unwrap(), // OR
+      Regex::new(r####"^(ε|epsilon)"####).unwrap(), // EPSILON
+      Regex::new(r####"^(\*)"####).unwrap(), // STAR
+      Regex::new(r####"^(\+)"####).unwrap(), // PLUS
+      Regex::new(r####"^(\?)"####).unwrap(), // QUESTION
+      Regex::new(r####"^(\()"####).unwrap(), // LPAREN
+      Regex::new(r####"^(\))"####).unwrap(), // RPAREN
+      Regex::new(r####"^('([^\a\d\n\r\t\f\v\\']|(\\\\|\\'|\\a|\\d|\\n|\\r|\\t|\\f|\\v|\\u\{(0x|0)?[a-f0-9]+\})|\d)*')"####).unwrap(), // STRING_LITERAL
+      Regex::new(r####"^(/(\\/|[^/])+/)"####).unwrap(), // REGULAR_LITERAL
+      Regex::new(r####"^([ \r\n\t\f]+)"####).unwrap(), // WHITE_SPACE
+    
     ];
-
+    
+    // 同样使用模板
     let token_names = vec![
-      "_START", "_STOP", "LINE_COMMENT", "BLOCK_COMMENT", "WS", "FRAGMENT", "TOKEN_REF",
-      "RULE_REF", "COLON", "OR", "SEMI", "STAR", "PLUS", "QUEStION", "DOT",
-      "NOT", "LPAREN", "RPAREN", "POUND", "STRING_LITERAL", "RANGE", "REGULAR_LITERAL", "EPSILON"
+      "_START", "_STOP", 
+      "RULE_REF", "TOKEN_REF", "COLON", "SEMI", "OR", "EPSILON", "STAR", "PLUS", "QUESTION", "LPAREN", "RPAREN", "STRING_LITERAL", "REGULAR_LITERAL", "WHITE_SPACE", 
     ];
 
     SyntaxisLexer { input: input.to_owned(), cursor: 0, regex_list, token_names, token_index: 0, position: Position { line: 0, char_position: 0 } }
@@ -87,7 +79,24 @@ impl SyntaxisLexer {
 impl Lexer for SyntaxisLexer {
 
 
-  fn scan(&mut self) -> Result<crate::runtime::token::Token, crate::runtime::lexer::Error> {
+  fn scan(&mut self) -> Result<Token, Error> {
+    if self.cursor > self.input.len() { return Err(Error {}) }
+    else if self.cursor >= self.input.len() {
+      self.cursor += 10;
+      return Ok(Token {
+        token_type: 1,
+        token_name: "_STOP".to_owned(),
+  
+        start: self.position.clone(),
+        stop: self.position.clone(),
+        
+        channel: 0,
+        text: "_STOP".to_owned(),
+        token_index: self.token_index,
+        char_start_index: self.cursor,
+        char_stop_index: self.cursor,
+      });
+    }
     let mut len = 0;
     let mut token_type = 0;
 
@@ -96,7 +105,7 @@ impl Lexer for SyntaxisLexer {
       if let Some(result) = result {
         if result.end() > len {
           len = result.end();
-          token_type = i + 2;
+          token_type = i+2;
         }
       }
     }
@@ -146,13 +155,14 @@ impl Lexer for SyntaxisLexer {
   fn scan_all_on_channel_tokens(&mut self, channel: usize) -> Vec<Token> {
     let mut result = Vec::new();
     while let Ok(token) = self.scan() {
-      if token.token_type == SyntaxisLexer::LINE_COMMENT || token.token_type == SyntaxisLexer::BLOCK_COMMENT
-        || token.token_type == SyntaxisLexer::WS { continue; }
+      if token.token_type == SyntaxisLexer::WHITE_SPACE { continue; }
       if token.channel == channel {
         result.push(token);
       }
     }
-    println!("{}", &self.input[self.cursor..]);
+    if self.cursor < self.input.len() {
+      println!("{}", &self.input[self.cursor..]);
+    }
     result
   }
 }
